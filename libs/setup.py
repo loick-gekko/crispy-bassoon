c8b1aec2b7fc6c597da112e253addc36f2113a8f
loick-gekko <40033514+loick-gekko@users.noreply.github.com>
2019-03-21 16:53:59 +0100

    Update setup.py
    Add the creation of logs files for the setup.py

--- libs/setup.py
+++ libs/setup.py
@@ -1,14 +1,20 @@
-#! python2.7
+
+
+# ! python2.7
 # coding=utf-8
 import requests
 import os
 import json
 import sys
+
 onboardserver = os.getenv('ONBOARD_SERVER')
 envUrl = "https://%s/" % onboardserver
 orgmanagerpassword = os.getenv('org_mngr_pass', '')
 orgmanagerid = os.getenv('org_mngr_user', '')
 portfolio_name = os.getenv('portfolio_name', '')
+uuId = uuid.uuid1()
+outputLogs =  "--------------------------------------------------------"
+
 ############################################################################ Start of preparation of Fonctions #############################################################################################
 
 def login(user=None, password=None):
@@ -29,6 +35,8 @@ def login(user=None, password=None):
         return headers
     else:
         print(r.status_code, r.content)
+        outputLogs += "\n" +"ERROR :"+ r.status_code + "   "+  r.content
+        createLogFile(outputLogs)
         sys.exit(1)
 
 def reqGet(header=None, url=None):
@@ -40,6 +48,8 @@ def reqGet(header=None, url=None):
 
     if requestGET.status_code != 200:
         print(requestGET.status_code, requestGET.content)
+        outputLogs += "\n" +"ERROR :"+ requestGET.status_code + "   "+  requestGET.content
+        createLogFile(outputLogs)
         sys.exit(1)
     else:
         return json.loads(requestGET.content)
@@ -54,32 +64,44 @@ def reqPOST(header=None, url=None, payload=None):
         return json.loads(r.content)
     else:
         print(r.status_code, r.content)
+        outputLogs += "\n" +"ERROR :"+ r.status_code + "   "+  r.content
+        createLogFile(outputLogs)
         sys.exit(1)
 
+def createLogFile(content=None):
+    print("log file is create with this name : bats-test-"+ test-uuId)
+    fichier = open("bats-test-"+ test-uuId, "a")
+    fichier.write(outputLogs)
+    fichier.close()
 ############################################################################ END of preparation of Fonctions #############################################################################################
 ############################################################################ Start of preparation of the script #############################################################################################
 
 print("--------------------------------------------------------")
 print("Try to get the token : ")
-header  = login(orgmanagerid , orgmanagerpassword )
-#print(header)
+header = login(orgmanagerid, orgmanagerpassword)
+outputLogs += "\n" +"Try to get the token : "
+outputLogs += "\n" + header
+outputLogs += "\n" +"--------------------------------------------------------"
 print("--------------------------------------------------------")
 print("Try to get orgs : ")
 listOrgs = reqGet(header, 'listoforgs')
-#print(listOrgs)
+outputLogs += "\n" + "Try to get orgs : "
+outputLogs += "\n" +listOrgs
+outputLogs += "\n" +"--------------------------------------------------------"
 print("--------------------------------------------------------")
 print("Try to get org 0: ")
-#print(listOrgs[0])
-listOrgs0=listOrgs[0]
-#print(listOrgs0['idOrg'])
+listOrgs0 = listOrgs[0]
+outputLogs += "\n" + "Try to get org 0 ID: "
+outputLogs += "\n" + listOrgs0['idOrg']
+outputLogs += "\n" +"--------------------------------------------------------"
 print("--------------------------------------------------------")
 print("get spec of this org")
-org = reqGet(header, 'getOrg/'+str(listOrgs0['idOrg']))
-#print(org)
-
-print("########################################################")
+org = reqGet(header, 'getOrg/' + str(listOrgs0['idOrg']))
+outputLogs += "\n" + "get spec of this org"
+outputLogs += "\n" + org
+outputLogs += "\n" +"--------------------------------------------------------"
 print("################ Lets create the PP ####################")
-print("########################################################")
+outputLogs += "\n" + "################ Lets create the PP ####################"
 ppToCreate = {
     "environment": "Homol",
     "createppname": portfolio_name,
@@ -101,8 +123,8 @@ ppToCreate = {
         "cluster": "Homol"
     }
 }
-#print("our pp look like this :")
-#print (ppToCreate)
+outputLogs += "\n" +"--------------------------------------------------------"
+outputLogs += "\n" + "our pp look like this :"
+outputLogs += "\n" + ppToCreate
 print("--------------------------------------------------------")
-pp= reqPOST(header,listOrgs0['nameOrg']+'/createpp',ppToCreate)
-#print(pp)
+pp = reqPOST(header, listOrgs0['nameOrg'] + '/createpp', ppToCreate)
